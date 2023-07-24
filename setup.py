702349c16722172fc1934816183578869f23ec22
5121eun <121006954+5121eun@users.noreply.github.com>
2023-07-24 19:03:40 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='rl',
-    packages = ['models'],
-    version='0.0.1',
+    packages = ['models', 'commons'],
+    version='0.0.2',
     url='https://github.com/5121eun/rl.git',
     install_requires=[
         'torch'
