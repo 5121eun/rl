482c0569b99df2d64431605dafa353dbb59a2752
5121eun <121006954+5121eun@users.noreply.github.com>
2023-07-24 19:00:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rl',
-    packages = ['rl'],
+    packages = ['models'],
     version='0.0.1',
     url='https://github.com/5121eun/rl.git',
     install_requires=[
